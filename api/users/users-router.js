abf16432fffb25ecd0e5d54e25bd3e272c711932
McNultyPT <mcnultypt@gmail.com>
2019-03-04 13:03:07 -0800

    writing post request to add a user with hashed password

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const knex = require('knex');
 const bcrypt = require('bcryptjs');
 
 const db = require('../../data/dbConfig.js');
@@ -7,6 +6,22 @@ const Users = require('./users-model.js');
 
 router.get('/', (req, res) => {
     res.send("It's Working!");
+});
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 12);
+
+    user.password = hash;
+
+    Users.add(user)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
