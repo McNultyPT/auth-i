00bbad6996ba63108876bae7694f72634aea25bc
McNultyPT <mcnultypt@gmail.com>
2019-03-04 13:56:48 -0800

    creating restricted-router and writing middleware to restrict access to a registered user

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users/users-model.js');
 
 const usersRouter = require('./users/users-router.js');
+const restrictedRouter = require('./users/restricted-router.js');
 
 const server = express();
 
@@ -10,6 +14,28 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api', usersRouter);
+function restricted (req, res, next) {
+    const { username, password } = req.body;
+
+    if (username && password) {
+        Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next();
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    } else {
+        res.status(400).json({ message: 'Please provide your credentials.' });
+    }
+}
+
+server.use('/api/', usersRouter);
+server.use('/api/restricted', restricted, restrictedRouter);
 
 module.exports = server;
