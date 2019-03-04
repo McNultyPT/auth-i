18c5a310ee67f7b672a122887b43af832035f48d
McNultyPT <mcnultypt@gmail.com>
2019-03-04 13:10:04 -0800

    writing post request to log in a registered user

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -24,4 +24,21 @@ router.post('/register', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `${user.username} is logged in.` });
+            } else {
+                res.status(401).json({ message: 'You shall not pass!' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
