4e56a2b7bf9a182a86c847ff45e1163519a248b7
McNultyPT <mcnultypt@gmail.com>
2019-03-04 14:09:59 -0800

    fixing punctuation and regorganizing server.js file

--- api/server.js
+++ api/server.js
@@ -14,28 +14,28 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use('/api/', usersRouter);
+server.use('/api/restricted', restricted, restrictedRouter);
+
 function restricted (req, res, next) {
     const { username, password } = req.body;
 
     if (username && password) {
         Users.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'You shall not pass!' });
-            }
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'You shall not pass!' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
     } else {
         res.status(400).json({ message: 'Please provide your credentials.' });
     }
 }
-
-server.use('/api/', usersRouter);
-server.use('/api/restricted', restricted, restrictedRouter);
 
 module.exports = server;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -8,17 +8,17 @@ function restricted (req, res, next) {
 
     if (username && password) {
         Users.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'You shall not pass!' });
-            }
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({ message: 'You shall not pass!' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
     } else {
         res.status(400).json({ message: 'Please provide your credentials.' });
     }
