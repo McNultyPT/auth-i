e4d11546ae9bf87d4950f4ba109069c2268282cc
McNultyPT <mcnultypt@gmail.com>
2019-03-05 13:56:40 -0800

    rewriting restricted global middleware in server.js and creating a /api/restricted route

--- api/users/restricted-router.js
+++ api/users/restricted-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Restricted Content');
+    res.send(`Today me will live in the moment, unless it's unpleasant, in which case me will eat a cookie. -Cookie Monster`);
 });
 
 module.exports = router;
